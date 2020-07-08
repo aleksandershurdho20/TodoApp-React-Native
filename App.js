651fdce2e905e12a61ee502700ef08bb8fef23a5
aleksander <aleksandershurdho@gmail.com>
2020-07-08 23:23:01 +0200

    Finished header layout

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
 import { StyleSheet, Text, View, Button,ScrollView,FlatList,TouchableOpacity } from 'react-native';
-
+import Header from './Components/Header'
 export default function App() {
-  
+  const [todos,setTodos]=useState([
+    {text:'buy coffee', key:'1'},
+    {text:'create app', key:'2'},
+    {text:'play playstation', key:'3'}
+
+
+  ])
   return (
     <View style={styles.container}>
-    
+      {/* header */}
+      <Header/>
+      <View style={styles.content}>
+        {/* Form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({item})=>(
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
 
     </View>
    
@@ -22,6 +40,12 @@ const styles = StyleSheet.create({
     paddingTop:40,
     paddingHorizontal:20
   },
+  content:{
+    padding:40
+  },
+  list:{
+    marginTop:20
+  }
   
   
 });
